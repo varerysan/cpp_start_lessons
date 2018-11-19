57ea2f0aab8dd264996583ad82623a4a4d94f643
varerysan <valery.mosyagin@gmail.com>
2018-11-20 01:03:09 +0300

    Add some classes

--- some_ideas/huffman/main.cpp
+++ some_ideas/huffman/main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 #include <fstream>
+#include <array>
 
+class Statistics
+{
+public:
+    std::array<int,256> stat;
+    
+    void clear()
+    {
+        for( const auto &s : stat) s = 0;
+    }
+
+    Statistics()
+    {
+        clear();
+    }
+    
+    
+    
+    void addData( const vector<uint_8> data )
+    {
+        clear();
+        for(const auto d : data)
+        {
+            stat[d]++;
+        }
+    }
+    
+    
+
+};
+
+class Processor
+{
+public:
+    Statistics statistics;
+    
+    const int max_size = 65536;
+    
+    vector<uint_8> buffer;
+    
+    Processor()
+    {
+        buffer.reserve(max_size);
+    
+    }
+    
+    void addFile(std::string name)
+    {
+        
+        char value;
+        while( file.get(value) && buffer.size() < max_size )
+        {
+            int v = value;
+            std::cout << "v=" << v << std::endl;
+        }
+    
+    }
+
+};
 
 void read()
 {
