6fa761e6b6a67dbf89445b1a1f64ab4c82886e92
varerysan <valery.mosyagin@gmail.com>
2018-11-20 00:39:39 +0300

    Add draft to read file by chars

--- some_ideas/huffman/main.cpp
+++ some_ideas/huffman/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <fstream>
+
+
+void read()
+{
+    std::ifstream file("data.txt", std::ios::binary);
+    if( ! file )
+    {
+        std::cout << "Can't open file" <<std::endl;
+        return;
+    }
+    
+    char value;
+    while( file.get(value) )
+    {
+        int v = value;
+        std::cout << "v=" << v << std::endl;
+    }
+
+}
 
 int main()
 {
     std::cout << "Hello Huffman" << std::endl;
+    read();
     return 0;
 }
 
